d02e7fb22e8028bcb44b0080d4b3aabf3ba70a98
djbavibeats <86330061+djbavibeats@users.noreply.github.com>
2024-03-19 10:16:51 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -143,7 +143,7 @@ function openMenu() {
       {/* <div className="bg-red-500 fixed w-fulls top-0 left-0 right-0 flex h-20 items-center justify-between"> */}
           {/* Share Icon */}
           <div className="text-center">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:cursor-pointer" onClick={ share }>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="block md:hidden w-6 h-6 hover:cursor-pointer" onClick={ share }>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
               </svg>  
           </div>
